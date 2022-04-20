b5dbc5a610a8c952716a92bbe1ca4732dd6c221b
张亚飞 <672233580@qq.com>
2022-04-20 14:00:16 +0800

    Initial Commit

---
+++ CombineTwoRequest0420/CombineTwoRequest0420App.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTwoRequest0420App.swift
+//  CombineTwoRequest0420
+//
+//  Created by 张亚飞 on 2022/4/20.
+//
+
+import SwiftUI
+
+@main
+struct CombineTwoRequest0420App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineTwoRequest0420/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineTwoRequest0420
+//
+//  Created by 张亚飞 on 2022/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
